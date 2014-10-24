51865033be109ac6ed4e35f5c08432e05605b322
pedromarins <contato@pedromarins.com>
2014-10-24 19:17:42 -0200

    add quero to api-docs page

--- api-docs.php
+++ api-docs.php
@@ -18,6 +18,35 @@
 
             <p class="api-code">http://gaivota.org/api.php?[PARAMETRO]=[VALOR]&[PARAMETRO2]=[VALOR]</p>
 
+            <p class="api-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Facere quod, recusandae alias. Harum optio eos corporis ratione eveniet omnis, tempore accusamus ullam id repellendus, quaerat veritatis voluptas earum sunt nemo.</p>
+
+            <ul class="api-query">
+                <li>
+                    <div>
+                        <p class="query-description">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quo illo consequuntur.</p>
+                        <p class="query-param">station_id</p>
+                        <p class="query-volume">station number</p>
+                    </div>
+                    <p class="query-url"><span>Exemplo:</span> <a href="http://gaivota.org/api.php?station_id=1">http://gaivota.org/api.php?station_id=1</a></p>
+                </li>
+                <li>
+                    <div>
+                        <p class="query-description">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quo illo consequuntur.</p>
+                        <p class="query-param">volume</p>
+                        <p class="query-volume">mostrecent</p>
+                    </div>
+                    <p class="query-url"><span>Exemplo:</span> <a href="http://gaivota.org/api.php?volume=mostrecent">http://gaivota.org/api.php?volume=mostrecent</a></p>
+                </li>
+                <li>
+                    <div>
+                        <p class="query-description">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quo illo consequuntur.</p>
+                        <p class="query-param">type</p>
+                        <p class="query-volume">air<br>water<br>boia</p>
+                    </div>
+                    <p class="query-url"><span>Exemplo:</span> <a href="http://gaivota.org/api.php?type=air">http://gaivota.org/api.php?type=air</a></p>
+                </li>
+            </ul>
+
         </div>
     </section>
 
