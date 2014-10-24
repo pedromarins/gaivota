419fb750f155f0328508a16cde102bb62e9d0500
pedromarins <contato@pedromarins.com>
2014-10-24 11:13:50 -0200

    moving things to footer file

---
+++ index.php
@@ -0,0 +1,8 @@
+<?php
+    $footer_extra = '
+    <script type="text/json" src="exemplo.json"></script>
+    <script src="js/jquery-2.1.1.min.js"></script>
+    <script src="js/geoPosition.js"></script>
+    <script src="js/map.js"></script>';
+    include("_footer.php")
+?>
