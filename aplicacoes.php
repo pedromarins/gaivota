e3ca4f7ad4ac10b1725c23b26d1a36f62ccc6fd3
pedromarins <contato@pedromarins.com>
2014-10-24 11:17:55 -0200

    css for application page

---
+++ aplicacoes.php
@@ -0,0 +1,29 @@
+<?php include("_header.php") ?>
+
+    <main>
+        <section class="banner application">
+        	<h2>Aplicações</h2>
+        	<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Excepturi accusantium molestias, ut dolorum dolor officiis obcaecati sequi rem eligendi necessitatibus pariatur, dignissimos numquam nostrum iure corporis cumque natus. Amet, minima.</p>
+        </section>
+        <section class="content">
+        	<ul class="application-content">
+        		<li>
+        			<img src="http://placehold.it/200x200" alt="">
+        			<h3>Agrucultura industrial e caseira</h3>
+        			<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Praesentium temporibus molestiae consequatur alias illo, inventore provident atque in dolor tempora. Sed voluptatibus quam illo quod iusto laudantium amet recusandae earum. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatem nihil, illo neque voluptatibus, explicabo quod a facere praesentium omnis enim, provident perferendis eos sequi impedit laboriosam, reiciendis. Inventore.</p>
+        		</li>
+        		<li>
+        			<img src="http://placehold.it/200x200" alt="">
+        			<h3>Defesa Civíl</h3>
+        			<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Praesentium temporibus molestiae consequatur alias illo, inventore provident atque in dolor tempora. Sed voluptatibus quam illo quod iusto laudantium amet recusandae earum. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatem nihil, illo neque voluptatibus, explicabo quod a facere praesentium omnis enim, provident perferendis eos sequi impedit laboriosam, reiciendis. Inventore.</p>
+        		</li>
+        		<li>
+        			<img src="http://placehold.it/200x200" alt="">
+        			<h3>Mergulho</h3>
+        			<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Praesentium temporibus molestiae consequatur alias illo, inventore provident atque in dolor tempora. Sed voluptatibus quam illo quod iusto laudantium amet recusandae earum. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatem nihil, illo neque voluptatibus, explicabo quod a facere praesentium omnis enim, provident perferendis eos sequi impedit laboriosam, reiciendis. Inventore.</p>
+        		</li>
+        	</ul>
+        </section>
+    </main>
+
+<?php include("_footer.php") ?>
