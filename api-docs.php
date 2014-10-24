a7eef7ce5fd5837f459ee93f650345af3873eb24
pedromarins <contato@pedromarins.com>
2014-10-24 18:13:02 -0200

    moving api to api-docs

---
+++ api-docs.php
@@ -0,0 +1,24 @@
+<?php include("_header.php") ?>
+
+    <section class="banner api">
+        <h2>API</h2>
+    </section>
+    <section class="content-block api-content">
+    	<div>
+        	<h3 class="content-title">Para que serve?</h3>
+            <p class="api-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Facere quod, recusandae alias. Harum optio eos corporis ratione eveniet omnis, tempore accusamus ullam id repellendus, quaerat veritatis voluptas earum sunt nemo.</p>
+
+            <h3 class="content-title">Como acessar?</h3>
+            <p class="api-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Facere quod, recusandae alias. Harum optio eos corporis ratione eveniet omnis, tempore accusamus ullam id repellendus, quaerat veritatis voluptas earum sunt nemo.</p>
+
+            <p class="api-code">http://gaivota.org/api.php</p>
+
+            <p class="api-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Facere quod, recusandae alias. Harum optio eos corporis ratione eveniet omnis, tempore accusamus ullam id repellendus, quaerat veritatis voluptas earum sunt nemo.</p>
+            <p class="api-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Facere quod, recusandae alias. Harum optio eos corporis ratione eveniet omnis, tempore accusamus ullam id repellendus, quaerat veritatis voluptas earum sunt nemo.</p>
+
+            <p class="api-code">http://gaivota.org/api.php?[PARAMETRO]=[VALOR]&[PARAMETRO2]=[VALOR]</p>
+
+        </div>
+    </section>
+
+<?php include("_footer.php") ?>
