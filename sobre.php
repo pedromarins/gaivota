c254facbff7df6777e6f9946c35f1c1de7b599c5
pedromarins <contato@pedromarins.com>
2014-10-24 12:23:32 -0200

    about page layout

---
+++ sobre.php
@@ -0,0 +1,18 @@
+<?php include("_header.php") ?>
+
+    <main>
+        <section class="banner about">
+            <h2>Sobre</h2>
+        </section>
+        <section class="content">
+        	<div class="content-about">
+	        	<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Excepturi accusantium molestias, ut dolorum dolor officiis obcaecati sequi rem eligendi necessitatibus pariatur, dignissimos numquam nostrum iure corporis cumque natus. Amet, minima. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Repellat accusantium sint quis cumque consectetur, dolores eveniet at quia quaerat debitis eos veniam, nulla quae officia, eaque quam, non quibusdam impedit.</p>
+
+	        	<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Excepturi accusantium molestias, ut dolorum dolor officiis obcaecati sequi rem eligendi necessitatibus pariatur, dignissimos numquam nostrum iure corporis cumque natus. Amet, minima.</p>
+
+	        	<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Excepturi accusantium molestias, ut dolorum dolor officiis obcaecati sequi rem eligendi necessitatibus pariatur, dignissimos numquam nostrum iure corporis cumque natus. Amet, minima.</p>
+	        </div>
+        </section>
+    </main>
+
+<?php include("_footer.php") ?>
