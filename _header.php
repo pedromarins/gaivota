e175080d46fec292d2b7859044b8495df9d70e87
pedromarins <contato@pedromarins.com>
2014-10-24 11:14:02 -0200

    moving thing to header file

---
+++ _header.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Gaivota</title>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width" />
+        <link rel="stylesheet" href="css/reset.css">
+        <link rel="stylesheet" href="css/style.css">
+        <link href='http://fonts.googleapis.com/css?family=Yanone+Kaffeesatz:400,300' rel='stylesheet' type='text/css'>
+		<?= @$header_extra ?>
+    </head>
+
+    <body>
+		<header>
+		    <h1><a href="#" class="logo">Gaivota</a></h1>
+		    <nav class="menu">
+		        <ul>
+		            <li><a href="sobre.php">sobre</a></li>
+		            <li><a href="aplicacoes.php">aplicações</a></li>
+		            <li><a href="colabore.php">colabore</a></li>
+		        </ul>
+		    </nav>
+		</header>
--- index.php
+++ index.php
@@ -1,4 +1,8 @@
 <?php
+    $header_extra = '<script type="text/javascript" src="http://maps.googleapis.com/maps/api/js?key=AIzaSyBhRXyrySdaevwUwTC4Vm_wugu8f917dQU&sensor=true">
+        </script>';
+    include("_header.php")
+?>
     $footer_extra = '
     <script type="text/json" src="exemplo.json"></script>
     <script src="js/jquery-2.1.1.min.js"></script>
