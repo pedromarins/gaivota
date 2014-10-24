d1ebd758be55344588fdcef5e4328963b864da35
henriquecrang <henrique@uniriotec.br>
2014-10-24 13:49:38 -0700

    adicionando conector banco de dados

---
+++ db.php
@@ -0,0 +1,15 @@
+<?php
+$servername = "localhost";
+$username = "";
+$password = "";
+$dbname = "gaivota";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password,$dbname);
+
+// Check connection
+if ($conn->connect_error) {
+ die("Não foi possível se conectar à base de dados. Favor entrar em contato com o suporte da Gaivota.<br>Erro: " . $conn->connect_error);
+}
+//echo "Connected successfully";
+?> 
