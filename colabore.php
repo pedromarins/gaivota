cd13cc08191d39f7ad07a5c14fd3448541d1e34b
pedromarins <contato@pedromarins.com>
2014-10-24 17:24:47 -0200

    share page

--- colabore.php
+++ colabore.php
@@ -4,6 +4,114 @@
         <h2>Colabore</h2>
         <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Excepturi accusantium molestias, ut dolorum dolor officiis obcaecati sequi rem eligendi necessitatibus pariatur, dignissimos numquam nostrum iure corporis cumque natus. Amet, minima.</p>
     </section>
-    <section class="content"></section>
+    <section class="share buy-list">
+    	<h3 class="content-title">Compre sua estação</h3>
+    	<p class="content-text">Interessado em ter uma estação Gaivota?</p>
+    	<ul class="buy-itens">
+    		<li class="buy-item">
+    			<p class="buy-item-title"><img src="img/marker.png" alt=""> boia de monitoramento</p>
+    			<ul>
+    				<li>
+    					<p>Sensor de temperatura</p>
+    				</li>
+					<li>
+						<p>Sensor de humidade relativa do ar</p>
+					</li>
+					<li>
+						<p>Sensor de temperatura do ambiente</p>
+					</li>
+					<li>
+						<p>Sensor de temperatura da agua</p>
+					</li>
+					<li>
+						<p>Turbidimetro</p>
+					</li>
+					<li>
+						<p>Barômetro</p>
+					</li>
+					<li>
+						<p>Anemometro</p>
+					</li>
+    			</ul>
+    		</li>
+    		<li class="buy-item">
+    			<p class="buy-item-title"><img src="img/marker.png" alt=""> monitoramento fixo</p>
+    			<ul>
+    				<li>
+    					<p>Sensor de temperatura</p>
+    				</li>
+					<li>
+						<p>Sensor de humidade relativa do ar</p>
+					</li>
+					<li>
+						<p>Sensor de temperatura do ambiente</p>
+					</li>
+					<li>
+						<p>Sensor de temperatura do solo</p>
+					</li>
+					<li>
+						<p>Sensor de humidade de solo</p>
+					</li>
+					<li>
+						<p>Barômetro</p>
+					</li>
+					<li>
+						<p>Anemometro</p>
+					</li>
+					<li>
+						<p>Pluviometro</p>
+					</li>
+					<li>
+						<p>Sensor de particulas no ar</p>
+					</li>
+					<li>
+						<p>Sensor de monóxico de carbono</p>
+					</li>
+    			</ul>
+    		</li>
+    		<li class="buy-item">
+    			<p class="buy-item-title"><img src="img/marker.png" alt=""> monitoramento móvel</p>
+    			<ul>
+    				<li>
+    					<p>Sensor de temperatura</p>
+    				</li>
+					<li>
+						<p>Sensor de humidade relativa do ar</p>
+					</li>
+					<li>
+						<p>Sensor de temperatura do ambiente</p>
+					</li>
+					<li>
+						<p>Barômetro</p>
+					</li>
+					<li>
+						<p>Anemometro</p>
+					</li>
+					<li>
+						<p>Pluviometro</p>
+					</li>
+					<li>
+						<p>Sensor de particulas no ar</p>
+					</li>
+					<li>
+						<p>Sensor de monóxico de carbono</p>
+					</li>
+					<li>
+						<p>Geolocalização</p>
+					</li>
+    			</ul>
+    		</li>
+    	</ul>
+    </section>
+    <section class="share your-own">
+        <h3 class="content-title">Monte sua estação</h3>
+    	<p class="content-text">Quer aprender como montar a sua estação por conta própria?</p>
+    	<a href="#" class="download">Faça o download do tutorial</a>
+    </section>
+    <section class="share send-data">
+    	<h3 class="content-title">Envie seus dados</h3>
+    	<p class="content-text">Já tem sua estação e quer colaborar com o mundo?</p>
+    	<a href="#" class="register">Cadastre sua estação</a>
+    </section>
 
 <?php include("_footer.php") ?>
