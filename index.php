453bf23e9360f6252803fe13742fdf1408d277fe
pedromarins <contato@pedromarins.com>
2014-10-24 11:14:15 -0200

    moving to php

--- index.php
+++ index.php
@@ -3,6 +3,32 @@
         </script>';
     include("_header.php")
 ?>
+
+    <main>
+        <div id="map_canvas">
+            <p class="loading">Carregando mapa</p>
+        </div>
+
+        <div class="helper">
+            <p>legenda</p>
+            <ul>
+                <li>
+                    <p>boia</p>
+                </li>
+                <li>
+                    <p>fixa</p>
+                </li>
+                <li>
+                    <p>móvel</p>
+                </li>
+                <li>
+                    <p>externa</p>
+                </li>
+            </ul>
+        </div>
+    </main>
+
+<?php
     $footer_extra = '
     <script type="text/json" src="exemplo.json"></script>
     <script src="js/jquery-2.1.1.min.js"></script>
