76a0f2b8497d5cc751e7a1457f8d4a0b288a5e85
pedromarins <contato@pedromarins.com>
2014-10-25 17:23:55 -0200

    texto de aplicacoes adicionado

--- aplicacoes.php
+++ aplicacoes.php
@@ -2,35 +2,35 @@
 
     <section class="banner application">
     	<h2>Aplicações</h2>
-    	<p>Os dados fornecidos pela Gaviota podem ser utilizados pelos mais diversos campos. Com informações ambientais geolocalizadas sendo fornecidas ao vivo ....  Lorem ipsum dolor sit amet, consectetur adipisicing elit. Excepturi accusantium molestias, ut dolorum dolor officiis obcaecati sequi rem eligendi necessitatibus pariatur, dignissimos numquam nostrum iure corporis cumque natus. Amet</p>
+    	<p>Os dados fornecidos pela Gaviota podem ser utilizados pelos mais diversos campos. Com informações ambientais geolocalizadas sendo fornecidas ao vivo .</p>
     </section>
     <section class="content">
     	<ul class="application-content">
     		<li>
     			<img src="img/agricultura.jpg" alt="Autor: Buchholz Pumpen fonte: http://commons.wikimedia.org/wiki/File:Buchholz_Beregnung_f%C3%BCr_Au%C3%9Fenreitpl%C3%A4tze.jpg Licença: CC-BY-SA 3.0" title="Autor: Buchholz Pumpen fonte: http://commons.wikimedia.org/wiki/File:Buchholz_Beregnung_f%C3%BCr_Au%C3%9Fenreitpl%C3%A4tze.jpg Licença: CC-BY-SA 3.0">
     			<h3>Agricultura industrial e familiar</h3>
-    			<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Praesentium temporibus molestiae consequatur alias illo, inventore provident atque in dolor tempora. Sed voluptatibus quam illo quod iusto laudantium amet recusandae earum. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatem nihil, illo neque voluptatibus, explicabo quod a facere praesentium omnis enim, provident perferendis eos sequi impedit laboriosam, reiciendis. Inventore.</p>
+    			<p>Estações fixa monitorando umidade do ar, do solo, temperatura e luminosidade podem controlar sistemas de irrigação e gerar alertas em situações críticas.</p>
     		</li>
             <li>
                 <img src="img/petroleo.jpg" alt="Autor: Russian.dissident Fonte: https://commons.wikimedia.org/wiki/File:LUN-A.jpg Licença: CC-0" title="Autor: Russian.dissident Fonte: https://commons.wikimedia.org/wiki/File:LUN-A.jpg Licença: CC-0">
                 <h3>Monitoramento de impacto ambiental</h3>
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Praesentium temporibus molestiae consequatur alias illo, inventore provident atque in dolor tempora. Sed voluptatibus quam illo quod iusto laudantium amet recusandae earum. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatem nihil, illo neque voluptatibus, explicabo quod a facere praesentium omnis enim, provident perferendis eos sequi impedit laboriosam, reiciendis. Inventore.</p>
-            </li>
+                <p>Estações flutuantes podem monitorar a qualidade da água do mar em torno de plataformas de petróleo ou de um rio ao longo de seu curso próxima de grandes industrias para detectar possíveis vazamentos e/ou descartes irregulares.</p>
+                </li>
             <li>
                 <img src="img/queimada.jpg" alt="Autor: Nelson Shannon Fonte: https://commons.wikimedia.org/wiki/File:Prescribed_forest_burn.jpg Licença: Domínio Público" title="Autor: Nelson Shannon Fonte: https://commons.wikimedia.org/wiki/File:Prescribed_forest_burn.jpg Licença: Domínio Público">
                 <h3>Detecção de queimadas</h3>
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Praesentium temporibus molestiae consequatur alias illo, inventore provident atque in dolor tempora. Sed voluptatibus quam illo quod iusto laudantium amet recusandae earum. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatem nihil, illo neque voluptatibus, explicabo quod a facere praesentium omnis enim, provident perferendis eos sequi impedit laboriosam, reiciendis. Inventore.</p>
+                <p>Estações fixas com detectores de fumaça pode detectar e gerar alertas de queimadas mesmo em regiões onde nenhum humano esteja presente.</p>
             </li>
     		<li>
     			<img src="img/deslisamento.jpg" alt="Autor: Liz Roll Fonte: https://commons.wikimedia.org/wiki/File:Hurricane_Gaston_landslide_damage.jpg Licença: Domínio Público" title="Autor: Liz Roll Fonte: https://commons.wikimedia.org/wiki/File:Hurricane_Gaston_landslide_damage.jpg Licença: Domínio Público">
     			<h3>Previsão de catástrofes</h3>
-    			<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Praesentium temporibus molestiae consequatur alias illo, inventore provident atque in dolor tempora. Sed voluptatibus quam illo quod iusto laudantium amet recusandae earum. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatem nihil, illo neque voluptatibus, explicabo quod a facere praesentium omnis enim, provident perferendis eos sequi impedit laboriosam, reiciendis. Inventore.</p>
-    		</li>
+    			<p>Estações fixas podem monitorar a umidade do solo em areas de risco, gerando alertas de perigo em momentos críticos.</p>
+                    </li>
     		<li>
     			<img src="img/turismo.jpg" alt="Autor: Marcelo Calvet Fonte: https://commons.wikimedia.org/wiki/File:Lagoa_Chico_Mendes.jpg Licença: CC-BY-SA 3.0" title="Autor: Marcelo Calvet Fonte: https://commons.wikimedia.org/wiki/File:Lagoa_Chico_Mendes.jpg Licença: CC-BY-SA 3.0">
     			<h3>Turismo</h3>
-    			<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Praesentium temporibus molestiae consequatur alias illo, inventore provident atque in dolor tempora. Sed voluptatibus quam illo quod iusto laudantium amet recusandae earum. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatem nihil, illo neque voluptatibus, explicabo quod a facere praesentium omnis enim, provident perferendis eos sequi impedit laboriosam, reiciendis. Inventore.</p>
-    		</li>
+    			<p>Estações flutuantes podem ser utilizadas em praias e pontos de mergulho para informar à turistas guias a qualidade da água ao vivo.</p> 
+                </li>
     	</ul>
     </section>
 
