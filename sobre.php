f457716b66ce00370d41ef1fe2496318063b6e58
henriquecrang <henrique@uniriotec.br>
2014-10-26 18:35:47 -0700

    adicionando medalha no sobre

--- sobre.php
+++ sobre.php
@@ -7,21 +7,21 @@
     	<div class="content-about">
             <p>Gaivota é uma rede colaborativa de dados climáticos. Desenvolvido em três camadas, o projeto possuí estações metereológicas feitas com Arduino, uma API que recebe e envia dados e visualização das informações armazenadas.</p>
 	    <p>Gaivota segue as filosofia do software livre, hardware livre, licenças livres e dados abertos. Assim, todo desenvolvimento realizado pode ser reutilizado por outros projetos.</p>
-	    <p>Gaivota foi iniciado no <b>Hackathon SESI Cultural Digital</b> onde foi premiado em 1º lugar como melhor projeto.</p>
+	    <p><img src="img/first.png" alt="Autor: Oldbeeg Fonte: https://commons.wikimedia.org/wiki/File:First_Place_Blue_Ribbon.png Licença: CC-BY-SA 3.0" title="Autor: Oldbeeg Fonte: https://commons.wikimedia.org/wiki/File:First_Place_Blue_Ribbon.png Licença: CC-BY-SA 3.0">Gaivota foi iniciado no <b>Hackathon SESI Cultural Digital</b> onde foi premiado em 1º lugar como melhor projeto.</p>
 
 
         	<h3 class="content-title">Equipe</h3>
         	<ul class="station-type">
         		<li>
-        			<img src="img/henrique.jpg" alt="Crang">
+        			<img src="img/henrique.jpg" title="Crang">
         			<h4>Henrique Andrade<br>backend</h4>
         		</li>
         		<li>
-        			<img src="img/pedro.jpg" alt="Pedrin">
+        			<img src="img/pedro.jpg" title="Pedrin">
         			<h4>Pedro Marins<br>frontend</h4>
         		</li>
         		<li>
-        			<img src="img/rodrigo.jpg" alt="Abdula">
+        			<img src="img/rodrigo.jpg" title="Abdula">
         			<h4>Rodrigo Padula<br>hardware</h4>
         		</li>
         	</ul>
